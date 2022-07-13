e372d91fd1d148b1c8f5b1c30555bff00c13b375
Vanshika Pahwa <107309332+vanshika201901187@users.noreply.github.com>
2022-07-14 03:40:16 +0530

    Create main.py

---
+++ 100DaysOfCode/Day7/main.py
@@ -0,0 +1,51 @@
+import random
+import hangman_art
+import hangman_words
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+guesses = []
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guesses:
+      print(f"You have already guessed {guess}.")
+    else:
+      guesses.append(guess)
+      
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You made a wrong guess. You guessed {guess}. You will lose a life.")
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
